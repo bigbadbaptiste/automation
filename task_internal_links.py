9e110e90b593276a01e6f492f738edf8ae8d11d6
bigbadbaptiste <baptiste.hausmann@stylight.com>
2017-04-24 16:49:37 +0200

    Initiate SQL script definition

---
+++ task_internal_links.py
@@ -0,0 +1,9 @@
+# from slack command '/fetch internal links {TLD}' (5)
+# script executes a SQL query which differs if TLD changes (1)
+# send email to me with Google Spreadsheet (2)
+# Google Spreadsheet named 'YYYYMMDD - Internal Links - {TLD}' (3)
+# Google Spreadsheet saved in Google Folder: 'C. Internal Linking' (SEO Input) (4)
+
+
+
+if __name__ == '__main__':
